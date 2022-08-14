12717b56515a37037bfd97a0aae5f8eb6959a0f1
Jerry Jäppinen <eiskis@gmail.com>
2022-08-14 10:10:41 +0000

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,10 +1,2 @@
-import { componentPages } from './config'
-
 // This should be a list of local paths
-export default [
-
-  // Define dynamic routes here
-  ...componentPages.map((componentName) => {
-    return `components/${componentName}-c`
-  })
-]
+export default []
