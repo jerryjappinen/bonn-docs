90daaf6e8d35c1a4e1d8c80955812f642be3bf7e
Jerry Jäppinen <eiskis@gmail.com>
2022-09-04 16:31:46 +0200

    Local path

--- stores/useHygraph.js
+++ stores/useHygraph.js
@@ -2,7 +2,8 @@ import { defineStore } from 'pinia'
 
 import { hygraphProjectId } from '@/config'
 
-import hygraph from '../../bonn/stores/setup/hygraph'
+import hygraph from 'bonn/stores/setup/hygraph'
+// import hygraph from '../../bonn/stores/setup/hygraph'
 
 export default defineStore('hygraph', hygraph({
   // persist: true,
